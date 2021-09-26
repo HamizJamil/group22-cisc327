052d11e2a5882553578115161f13683fb0ced7d3
18wsk <18wsk@queensu.ca>
2021-09-26 16:33:27 -0400

    Updated Product Class Format and representation

--- sprint01-test.py
+++ sprint01-test.py
@@ -7,15 +7,9 @@ db.create_all()  # creating database and columns from sprint01
 ADMIN = User(id=0, username="WilliamKennedy-ADMIN", email="18wsk@queensu.ca")  # testing user entity
 
 iPhone11 = Product(product_ID=0, seller="WilliamKennedy-ADMIN", price=600, number_of_product=1,
-<<<<<<< HEAD
                    product_title="iPhone11",
                    product_description='Brand New Never Opened iPhone 11 with 250GB of storage', verified_buyer='NONE',
                    dollars_made=0, verified_buyer_reviews='NONE')
-=======
-                   product_title="iPhone11", product_description='Brand New Never Opened iPhone 11 with 250GB of '
-                                                                 'storage', verified_buyer='NONE', dollars_made=0,
-                   verified_buyer_reviews='NONE')
->>>>>>> will_branch
 
 db.session.add(ADMIN)
 db.session.add(iPhone11)  # adding rows to identified columns
