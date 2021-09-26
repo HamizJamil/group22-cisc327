b271e7cfbba211dd0a2302adaa2b5abdcf9afe7c
18wsk <18wsk@queensu.ca>
2021-09-26 17:08:09 -0400

    Updated Product Test and Query

--- sprint01.py
+++ sprint01.py
@@ -1,13 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__)) # accessing proper directory for db file
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # ignore overhead restrictions
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # ignore overhead restrictions
 db = SQLAlchemy(app)
 
-total_products = 0
-list_of_products = []
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -18,42 +21,44 @@ class User(db.Model):
         return '<User %r>' % self.username
 
 
-def getuniqueproductID():
-    global total_products
-    x = random.randint(0, list_of_products) # change this to database attributes that are passed in system
-    for i in range(list_of_products):
-        if x != list_of_products[i]:
-            return x
-
-
 class Product(db.Model):
-    def __init__(self, seller):
-        self.seller = seller
-        self.number_of_product = int(input("Please Enter the Amount of these products you would like to sell: "))
-        self.price = input("Please Enter a Price you would like to sell the product for: ")
-        self.product_ID = getuniqueproductID()
-        self.product_title = input("Please Enter a Product Title: ")
-        self.verified_buyer = []
-        self.dollars_made = 0
-        self.verified_buyer_reviews = []
+    """
+    Class to represent a product being added to "ebay" page by seller
 
-    def purchased(self, number_of_product, price):
-        temp = self.number_of_product
-        self.number_of_product -= number_of_product
-        self.dollars_made = (temp-self.number_of_product)*price
-
+    Attributes:
+        - Product_ID = to uniquely identify a product and for easy extraction from database structure
+        - Seller =  identifies what user created the product to be sold
+        - Price = the amount of money the seller wishes to sell the product for
+        - number_of_product = inventory of the seller for this specific product
+        - product_description = the qualitative and quantitative features of the product described by seller
+        - verified_buyer = list of Users who have purchased the product and has been confirmed by the system
+        - dollars_made = the accumulated amount of money from this product which will be used in banking functions
+        - verified_buyer_reviews = list of reviews made by confirmed verified_buyers
+    """
     product_ID = db.Column(db.Integer, primary_key=True)
     seller = db.Column(db.String(80), primary_key=True)
     price = db.Column(db.Integer, primary_key=True)
     number_of_product = db.Column(db.Integer, primary_key=True)
     product_title = db.Column(db.String(30), primary_key=True)
+    product_description = db.Column(db.String(150), primary_key=True)
     verified_buyer = db.Column(db.String(80), primary_key=True)
     dollars_made = db.Column(db.Integer, primary_key=True)
     verified_buyer_reviews = db.Column(db.String(150), primary_key=True)
 
-    def __repr__(self):
-        return '<Product: {}, {}, {}, {}, {}, {}, {}>'.format(self.product_ID, self.seller, self.price,
-                                                              self.number_of_product, self.product_title,
-                                                              self.verified_buyer, self.dollars_made,
-                                                              self.verified_buyer_reviews)
+    def __init__(self, product_ID, seller, price, number_of_product, product_title, product_description, verified_buyer,
+                 dollars_made, verified_buyer_reviews):
+        self.product_ID = product_ID
+        self.seller = seller
+        self.price = price
+        self.number_of_product = number_of_product
+        self.product_title = product_title
+        self.product_description = product_description
+        self.verified_buyer = verified_buyer
+        self.dollars_made = dollars_made
+        self.verified_buyer_reviews = verified_buyer_reviews
 
+    def __repr__(self):
+        return "<Product(product_title= {}, product_ID= {}, seller= {}, price= {}, number_of_product= {}, " \
+               "product_description= {}, verified_buyer= {}, dollars_made= {},verified_buyer_reviews= {})>".format(
+                self.product_title,self.product_ID, self.seller, self.price, self.number_of_product,
+                self.product_description, self.verified_buyer, self.dollars_made, self.verified_buyer_reviews)
