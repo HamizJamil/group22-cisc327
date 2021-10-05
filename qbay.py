1e7ea658d8daaab9c84846e9f7b8047092f5a3c5
18wsk <18wsk@queensu.ca>
2021-10-05 14:23:35 -0400

    Formatted Correctly For PEP8

--- qbay.py
+++ qbay.py
@@ -7,7 +7,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,'db.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+    os.path.join(basedir, 'db.sqlite')
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  
 # ignore overhead restrictions
@@ -44,7 +45,7 @@ class Product(db.Model):
     product_description = db.Column(db.String(150), primary_key=True)
 
     def __init__(self, product_ID, owner_email, price, number_of_product,
-     product_title, product_description):
+                 product_title, product_description):
         self.product_title = product_title
         self.product_ID = product_ID
         self.owner_email = owner_email
@@ -55,5 +56,6 @@ class Product(db.Model):
     def __repr__(self):
         return "<Product(product_title= {}, product_ID= {}, owner_email= {},\
             price= {}, number_of_product= {}, product_description= {})>"\
-            .format(self.product_title,self.product_ID, self.owner_email, 
-            self.price, self.number_of_product,  self.product_description)
+            .format(self.product_title, self.product_ID, self.owner_email, 
+                    self.price, self.number_of_product, 
+                    self.product_description)
--- qbay_test.py
+++ qbay_test.py
@@ -5,13 +5,14 @@ from qbay import Product
 db.create_all()  # creating database and columns from qbay
 
 ADMIN = User(id=0, username="WilliamKennedy-ADMIN", email="18wsk@queensu.ca") 
- # testing user entity
+# testing user entity
 
 iPhone11 = Product(product_ID=0, owner_email="WilliamKennedy-ADMIN", price=600, 
-    number_of_product=1, product_title="iPhone11",
-    product_description='Brand New iPhone 11 with 250GB of storage')
+                   number_of_product=1, product_title="iPhone11",
+                   product_description='Brand New iPhone 11 with" \
+                   "250GB of storage')
 
-db.session.add(ADMIN) # adding rows to identified columns
+db.session.add(ADMIN)  # adding rows to identified columns
 db.session.add(iPhone11)
 
 
