0c620f0b599916c4fabec98bd21b70ccb07ab540
18wsk <18wsk@queensu.ca>
2021-11-03 13:26:17 -0400

    Adding Smoke Screen Test For CreateProduct

--- qbay_test/frontend/test_createproduct.py
+++ qbay_test/frontend/test_createproduct.py
@@ -2,18 +2,42 @@ from seleniumbase import BaseCase
 from qbay_test.conftest import base_url
 from unittest.mock import patch
 from qbay.models import Product
+from selenium import webdriver
 
 
 class FrontEndProductCreationTest(BaseCase):
     
-    # Smoke Test For Create Product
+    # Smoke Test For Create Product - checking if submit and fill works
     def test_create_product_1(self, *_):
         self.open(base_url + '/createproduct')
         self.type("#user_email", "test0@test.com")
         self.type("#title", "Iphone 11 Max Pro")
         self.type("#price", 1000)
         self.type("#description", "Brand-New, so good and fast")
-        self.click('input[type="submit"]')
+        self.find_element("#Save").click()
+        # veryfing that it redirects to homepage
+        self.assert_title("Home Page")  
+        # verifying a product is successfully commited
+        new_prod = Product.query.filter_by(title="Iphone 11 Max Pro").all()
+        assert new_prod is not None 
+
+    def test_create_product_2(self, *_):
+        self.open(base_url + '/createproduct')
+        self.type("#user_email", "test0@test.com")
+        self.type("#title", "Super cool new phone")
+        self.type("#price", 1000)
+        self.type("#description", "You wish your phone was this good")
+        self.find_element("#Back").click()
+        # veryfing that it redirects to homepage
+        self.assert_title("Home Page")  
+        # verifying a product is successfully commited
+        new_prod = Product.query.filter_by(title="Super cool new phone").first()
+        assert new_prod is None 
+    
+
+    
+    
+        
     
 
 
--- qbay_test/test_models.py
+++ qbay_test/test_models.py
@@ -1,54 +1,67 @@
 from qbay.models import *
 
 
-# using assert keyword to run checks 
+# using assert keyword to run checks with global backend functions
 def test_r1_user_register():
+    # checking 3 different user situations where 3rd pw is missing special char
     assert register_user('u0X', 'test0@test.com', 'eA123456!') is True
     assert register_user('u0X', 'test1@test.com', 'eA123456!') is True
     assert register_user('u0X', 'test0@test.com', '123456') is False
 
 
-def test_r2_login():
+def test_r2_login():  
+    # checking a fake user login and verifying its username credential
     user = login('test0@test.com', 'eA123456!')
     assert user is not None
     assert (user.user_name == 'u0X') is True
-    
+    # testing login with wrong credentials
     user = login('test0@test.com', 'eA123456')
     assert user is None
 
 
 def test_r3_user_update():  
+    # taking passed users from r1 and and checking that the added addy/postal
+    # code is valid by returning a user object
     assert update_user('test0@test.com', 'u1X', '99 University Ave Kingston', 
                        'K7L 3N6') is not None
+    # testing with incorrect addy sequence resulting in no returned user
     assert update_user('test1@test.com', 'u1X', '9! University Ave Kingston ',
                        'K7L 3N6') is None
 
 
 def test_r4_product_create():
+    # testing creating a product with proper credentials
     assert create_product('iPhone11X New', 'Brand New iPhone11X 2020',
                           'test0@test.com', 600) is True
+    # testing creating a product with different correct credentials
     assert create_product('iPhone11X Pro', 'This iPhone is so powerful',
                           'test1@test.com', 1000) is True
+    # testing product failure with too small of description
     assert create_product('iPhone11X', ' New ', 'test0@test.com', 10) is False
 
 
 def test_r5_product_update():
+    # testing updating a prodcut with new description and price
     assert update_product('iPhone11X New', 'test0@test.com', 1000, 
                           "Coolest Phone Ever", None) is not None
     assert update_product('iPhone11X Pro', 'test1@test.com', 1500, None, 
                           "256GB storage and fast") is not None
+    # incorrectily updating product with price decrease
     assert update_product('iPhone11X New', 10001, "Coolest Phone Ever", None
                           ) is None
 
 
 def test_r6_create_review():
+    # creating a review with int score
     assert create_review('test1@test.com', 6, "This phone is so good") is True
+    # creating bad review with text score 
     assert create_review('test0@test.com', "Six", "This phone is so good"
                          ) is False
 
 
 def test_r7_create_transaction():
+    # check transaction with valid product
     product_bought = Product.query.filter_by(title='iPhone11X Pro'
                                              ).first()
     price = product_bought.price
-    assert create_transaction('test1@test.com', price)
+    assert create_transaction('test1@test.com', price) is True
