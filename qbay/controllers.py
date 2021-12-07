691b4b3421058ba88a3189f41e1af81c059aa16b
Hamiz Jamil <hamizj89@gmail.com>
2021-12-06 21:26:57 -0500

    added controllers function and HTML tweak

--- qbay/controllers.py
+++ qbay/controllers.py
@@ -7,14 +7,16 @@ from qbay import app
 from qbay.models import *
 
 
+# authenticates user currently in an active session with qBay
 def authenticate(req_email):
     authorized = True
     if "user_email" not in session or req_email != session["user_email"]:
-        flash("Uer is not authorized, please login")
+        flash("User is not authorized, please login")
         authorized = False
     return authorized
 
 
+# gets the login page for the user
 @app.route('/login', methods=['GET'])
 def login_get():
     if "user_email" in session:
@@ -22,6 +24,7 @@ def login_get():
     return render_template("login.html")
 
 
+# sends login information to backend and returns home page if successful
 @app.route('/login', methods=["POST"])
 def login_post():
     email = request.form["user_email"]
@@ -36,11 +39,13 @@ def login_post():
         return render_template("login.html")
 
 
+# gets the create product page for the user
 @app.route("/createproduct", methods=["GET"])
 def create_product_get():
     return render_template("createproduct.html")
 
 
+# sends new product information to backend to be added to DB
 @app.route("/createproduct", methods=["POST"])
 def create_product_post():
     user_email = request.form.get("user_email")
@@ -59,6 +64,8 @@ def create_product_post():
         return render_template("createproduct.html")
 
 
+# if POST request, sends registration information to backend DB
+# if GET request, returns HTML page for registration to the user
 @app.route("/registration", methods=["POST", "GET"])
 def registration():
     if request.method == "POST":
@@ -74,6 +81,7 @@ def registration():
         return render_template("registration.html")
 
 
+# removes user from logged in session and returns them to login page
 @app.route("/logout", methods=["GET"])
 def logout():
     session.pop("user_email", None)
@@ -81,6 +89,8 @@ def logout():
     return redirect("/login")
 
 
+# redirects users to login if they are not currently in an active
+# session, if they are it redirects them to the home page
 @app.route("/", methods=["GET"])
 def home():
     if "user_email" in session:
@@ -89,6 +99,8 @@ def home():
         return redirect("/login")
 
 
+# if POST request, sends updated profile info to backend DB
+# if GET request, returns update profile page for user
 @app.route("/updateprofile", methods=["POST", "GET"])
 def update_profile():
     if request.method == "POST":
@@ -107,11 +119,13 @@ def update_profile():
         return render_template("updateprofile.html")
 
 
+# returns update product page to the user
 @app.route("/updateproduct", methods=["GET"])
 def update_product_get():
     return render_template("updateproduct.html")
 
 
+# sends updated product information submitted by user to backend DB
 @app.route("/updateproduct", methods=["POST"])
 def update_product_post():
     title = request.form.get("title")
@@ -130,13 +144,28 @@ def update_product_post():
         return render_template("updateproduct.html")
 
 
+# redirects user to their homepage
 @app.route("/userhome", methods=["GET"])
 def user_home():
     return render_template("userhome.html")
 
 
+# redirects user to the product page for them to browse
 @app.route("/products", methods=["GET"])
 def products():
     email = session["user_email"]
     list_of_products = display_products(email)
     return render_template("products.html", query=list_of_products)
+
+
+# upon product purchase, sends product id and user to backend DB
+# and calls on create_transaction function to complete purchase
+@app.route("/buy_product/<int:product_id>/", methods=["GET"]) 
+def buy_product(product_id):
+    current_user = session["user_email"]
+    success = create_transaction(current_user, product_id, error_handler)
+    if success:
+        flash("Product successfully purchased")
+        return render_template("userhome.html")
+    else:
+        return redirect("/products")
